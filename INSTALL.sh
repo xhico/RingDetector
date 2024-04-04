5fadd29329d11f821a47d5f54af6ffb0f4e0fbfb
xhico <francgf@gmail.com>
2024-04-04 18:55:56 +0100

    Add Service

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,2 +1,5 @@
-# brew install portaudio
+#!/bin/bash
 sudo apt install portaudio19-dev -y
+sudo mv /home/pi/RingDetector/RingDetector.service /etc/systemd/system/ && sudo systemctl daemon-reload
+python3 -m pip install -r /home/pi/RingDetector/requirements.txt --no-cache-dir
+chmod +x -R /home/pi/RingDetector/*
