00ee000117c2d89f9c2b3bad9320f2c2f7a8d683
xhico <francgf@gmail.com>
2024-04-05 15:46:19 +0100

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo apt install portaudio19-dev -y
 sudo mv /home/pi/RingDetector/RingDetector.service /etc/systemd/system/ && sudo systemctl daemon-reload
 python3 -m pip install -r /home/pi/RingDetector/requirements.txt --no-cache-dir
 chmod +x -R /home/pi/RingDetector/*
