ad03755f64ac2f5807780bc2f07155abfcd783cf
xhico <francgf@gmail.com>
2024-04-06 02:10:20 +0100

    Update RingDetector.py

--- RingDetector.py
+++ RingDetector.py
@@ -1,11 +1,14 @@
+import csv
 import logging
 import os
 import signal
 import sys
+import time
 import traceback
 from collections import deque
 
-from utils import init_stream, close_stream
+import config
+from utils import init_stream, close_stream, get_volume
 
 
 def signal_handler(sig, frame):
@@ -34,20 +37,37 @@ def main():
     Main function to monitor microphone volume.
     """
 
+    # Read Saved Smooth Baseline
+    with open(config.saved_baseline_smooth_file) as in_file:
+        saved_baseline_smooth = [row for row in csv.DictReader(in_file)]
+
     # Initialize PyAudio stream for audio input.
     init_stream()
 
     # Log the start of recording volume data
     logger.info("Recording Volume Data")
 
-    # Initialize an empty list to store volume data
-    volume_data = []
+    # Initialize volume_data with a baseline
+    volume = get_volume()
+    volume_data = deque([{"counter": i, "volume": volume} for i in range(len(saved_baseline_smooth))], maxlen=len(saved_baseline_smooth))
 
-    # Record volume
-    data = deque(maxlen=5)
-    for i in range(20):
-        data.append(i)
-        logger.info(data)
+    # Record volume_data
+    counter = len(volume_data)
+    while True:
+        # Get the current volume
+        volume = get_volume()
+
+        # Log the current volume and timestamp
+        logger.info(f"{counter} | Volume - {volume}")
+
+        # Append timestamp and volume to the list
+        volume_data.append({"counter": counter, "volume": volume})
+
+        # Wait for X seconds before taking the next reading
+        time.sleep(0.001)
+
+        # Increase counter
+        counter += 1
 
 
 if __name__ == "__main__":
